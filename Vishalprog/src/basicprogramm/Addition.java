8e43fe171dd1d0a7a4019f83e7d935b74e305740
shubham deokar <sdeokar13599@gamil.comgit config --global user.name  shubham>
2020-09-06 11:46:58 +0530

    commit all

---
+++ Vishalprog/src/basicprogramm/Addition.java
@@ -0,0 +1,19 @@
+package basicprogramm;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		int number1=25;
+		int number2=15;
+		
+		int result=number1+number2;
+		System.out.println("The value of first number is:"+number1);
+		System.out.println("The value of second number is :"+number2);
+		
+		
+		System.out.println("The sum of given two number is:"+result);
+		
+
+	}
+
+}
---
+++ Vishalprog/src/basicprogramm/multiplication.java
@@ -0,0 +1,15 @@
+package basicprogramm;
+
+public class multiplication {
+
+	public static void main(String[] args) {
+		int number1=25;
+		int number2=15;
+		
+		int result=number1*number2;
+		
+		System.out.println("The multipliction of two number is :"+result);
+
+	}
+
+}
